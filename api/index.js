3fd7c06ae549ced38618ab08a46e1f923e81efce
champhysics@gmail.com <your_new_email@example.com>
2023-12-30 13:39:50 +0530

    mongodb connected successfully

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log(" Mongodb Connection is Successfully ")
+}).catch((error)=>{
+    console.log(`This is the error: {error}`)
+})
 const app=express();
 
 app.listen(3000,()=>{
