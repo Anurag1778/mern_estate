6b1973d27e32f5e3609ef9eb7d5d02a1e64b0177
champhysics@gmail.com <your_new_email@example.com>
2024-01-05 05:46:56 +0530

    solve the profile error

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,9 +15,10 @@ const userSchema =new mongoose.Schema({
         type:String,
         required:true,
         
-    },avatar:{
-type:String,
-default:"https://imgs.search.brave.com/UfByHaUWn4fefCd8wp5-WiFM1xqB_Vtkt6NdKl2XKuY/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0LzgzLzkwLzk1/LzM2MF9GXzQ4Mzkw/OTU2OV9PSTRMS05l/RmdId3Z2Vmp1NjBm/ZWpMZDlnajQzZElj/ZC5qcGc"
+    },
+    avatar:{
+         type:String,
+        default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     }
     
 },{timestamps:true});
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,27 @@
 import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link ,useNavigate} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useEffect,useState } from 'react'
 export const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className='bg-slate-900 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -9,15 +30,28 @@ export const Header = () => {
         <span className='text-green-700'><span className='text-red-800'>E</span>state</span>
         </h1> 
 </Link>
-        <form action="" className='bg-slate-100 p-2 rounded-lg flex items-center' >
-            <input type="text"  placeholder='Search... ' className='bg-transparent focus:outline-none w-24 sm:w-64 '/>
-            <FaSearch className="text-green-600"/>
+        <form onSubmit={handleSubmit} className='bg-slate-100 p-2 rounded-lg flex items-center' >
+            <input type="text"  placeholder='Search... ' className='bg-transparent focus:outline-none w-24 sm:w-64 '  value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}/>
+           <button>
+            <FaSearch className='text-slate-600' />
+          </button>
         </form>
 
         <ul className='flex gap-4'>
          <Link to="/">   <li className='text-white hidden sm:inline hover:underline'>Home</li></Link>
          <Link to="/about"><li  className='text-white  hidden sm:inline hover:underline'>About</li></Link>
-         <Link to="sign-in"><li  className='text-white sm:inline hover:underline'>Sign in</li></Link>
+         <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+          </Link>
         </ul>
         </div>
       </header>
